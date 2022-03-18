ffc5c0935ef803b32f84dc2a7b8f69c32c5392b1
masayuki kamata <masap@kamatatadashiyoshiterunoMacBook-Air.local>
2022-03-18 12:16:00 +0900

    controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    
+    def hello
+        render html: "hollaaaaaaaa world!"
+    end
+
+    def goodbye
+        render html: "good bye!"
+    end
 end
